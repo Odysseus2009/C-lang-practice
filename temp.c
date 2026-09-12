e59d6a50c9121c2bc4a974d85f3728be1e848bd1
Odysseus2009 <solidbranch3525@gmail.com>
2026-09-12 20:15:24 +0530

    Add files via upload
    This was the first code of mine For the conversion of Celcius to Farenheit.

---
+++ temp.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+	float celsius, farenheit;
+	
+	printf("Enter the Temperature Vlaue in Celsius:");
+	scanf("%f, \n", &celsius);
+	
+	farenheit = celsius + 9/5 + 32;
+	
+	printf("The Temperature in Farenheit is:");
+	printf("%f, \n", farenheit);
+	
+	return(0);
+	
+		
+	
+	
+	
+	
+	
+	 
+	
+	
+	
+	
+	
+	}
+
