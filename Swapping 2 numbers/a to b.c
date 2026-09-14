38408e13d458d6bbfcc3d1562305d783e3993c2f
Odysseus2009 <solidbranch3525@gmail.com>
2026-09-14 21:10:54 +0530

    Add files via upload

---
+++ Swapping 2 numbers/a to b.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main (){
+	
+	float A, B;
+	
+	printf("Enter the Value for A:");
+	scanf("%f, \n", &B);
+	
+	printf("Enter the value for B");
+	scanf("%f, \n", &A);
+	
+	printf("------The Results after swapping are:-----, \n");
+	printf("The Value of A =,%f, \n", A);
+	printf("The Value of B =, %f, \n", B);
+	
+	return(0);
+	
+	}
